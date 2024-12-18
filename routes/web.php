5b111afb906f4b19682494a3e7e8ff45dedfd4a0
Diveshbari0512 <diveshbari2000@gmail.com>
2024-12-18 16:02:21 +0530

    revert

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,3 @@ require __DIR__.'/auth.php';
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
